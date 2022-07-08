dbb2bda25adaeecadfd68f99e222257482def414
Tyson Mahlangu <siphotysonmahlangu@gmail.com>
2022-07-08 11:34:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess;
+using System.Data;
+using System.Data.SqlClient;
+
+string connect ="Server=(localdb)\\MSSQLLocalDB;Trusted_Connection=True;Initial Catalog=ItemDB";
+
+SqlAccess.Connect(connect);
+
+
+
